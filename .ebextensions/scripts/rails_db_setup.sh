935fabfc3974cde28f8c949de28f93f7700c1f87
John Goldsmith <johngoldsm1@gmail.com>
2018-01-05 14:58:36 -0800

    build(aws): Add Elastic Beanstalk extension script to automatically run database setup

---
+++ .ebextensions/scripts/rails_db_setup.sh
@@ -0,0 +1,16 @@
+if [ "${PROCESS}" = "WEB" ]; then
+
+  . /opt/elasticbeanstalk/hooks/common.sh
+
+  EB_SUPPORT_FILES=$(/opt/elasticbeanstalk/bin/get-config container -k support_files_dir)
+
+  EB_CONFIG_DOCKER_ENV_ARGS=()
+
+  while read -r ENV_VAR; do
+    EB_CONFIG_DOCKER_ENV_ARGS+=(--env "$ENV_VAR")
+  done < <($EB_SUPPORT_FILES/generate_env)
+
+  echo "Running database setup for aws_beanstalk/staging-app..."
+  docker run --rm "${EB_CONFIG_DOCKER_ENV_ARGS[@]}" aws_beanstalk/staging-app bundle exec rails db:setup || echo "Database setup failed."
+fi
+true
